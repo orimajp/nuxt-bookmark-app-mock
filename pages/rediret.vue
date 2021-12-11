a527df9fe01dd8c0cb36b57119fc03b0a5cf0283
Hideo Oshima <orimajp@gmail.com>
2021-12-11 20:17:45 +0900

    リロード時404エラー回避テスト

---
+++ pages/rediret.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent, useRoute, useRouter} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  layout: 'plain',
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+
+    const param = route.value.query.p
+    if (!param) {
+      return router.replace('/')
+    }
+    const redirectPath = '/' + param
+    router.replace(redirectPath)
+  }
+})
+</script>
